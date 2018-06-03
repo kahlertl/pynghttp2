c66a49cedb15124987ad76ad02dbd181bf09401c
Lucas Kahlert <lucas.kahlert@square-src.de>
2018-06-03 23:23:13 +0200

    Bumps version to v0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='pynghttp2',
-    version='0.3.0',
+    version='0.3.1',
     platforms=['POSIX/Unix'],
     description='Asyncio bindings for libnghttp2 based on ctypes',
     long_description=read('README.rst'),
